cc3b5a9a0b0db0c6a7ff0f33bc99796ea56873b7
ItsJackAnton <jacksa999@hotmail.com>
2024-01-15 16:59:35 -0500

    Setting up first world action "battle_outcome_won"
    This action is called when the player wins the battle

--- Assets/_ProjectAssets/Scripts/LuckyWheel/LuckyWheelUI.cs
+++ Assets/_ProjectAssets/Scripts/LuckyWheel/LuckyWheelUI.cs
@@ -5,6 +5,11 @@ using UnityEngine.UI;
 using Photon.Pun;
 using Newtonsoft.Json;
 using TMPro;
+using System.Collections.Generic;
+using Candid.World.Models;
+using Candid;
+using Boom.Utility;
+using static EntityFieldEdit;
 
 public class LuckyWheelUI : MonoBehaviour
 {
@@ -26,23 +31,151 @@ public class LuckyWheelUI : MonoBehaviour
 
     public async void RequestReward()
     {
-        int _rewardId = -1;
+        //int _rewardId = -1;
+        //try
+        //{
+        //    string resp = await NetworkManager.GETRequestCoroutine("/leaderboard/spin-the-wheel?matchId=" + PhotonNetwork.CurrentRoom.Name,
+        //    (code, err) =>
+        //    {
+        //        Debug.LogWarning($"Failed to get reward type {err} : {code}");
+        //    }, true);
+
+        //    LuckyWheelRewardResponse _response = JsonConvert.DeserializeObject<LuckyWheelRewardResponse>(resp);
+        //    _rewardId = _response.RewardType;
+        //}
+        //catch
+        //{
+        //    _rewardId = 1;
+        //}
+
+        //choosenReward = LuckyWheelRewardSO.Get(_rewardId);
+        //if (requestedToSeeReward)
+        //{
+        //    Setup();
+        //}
+
         try
         {
-            string resp = await NetworkManager.GETRequestCoroutine("/leaderboard/spin-the-wheel?matchId=" + PhotonNetwork.CurrentRoom.Name,
-            (code, err) =>
-            {
-                Debug.LogWarning($"Failed to get reward type {err} : {code}");
-            }, true);
+            "Spin Wheel of Fortune".Log(typeof(LuckyWheelUI).Name);
+            //Action Id
+            var actionId = "battle_outcome_won";
 
-            LuckyWheelRewardResponse _response = JsonConvert.DeserializeObject<LuckyWheelRewardResponse>(resp);
-            _rewardId = _response.RewardType;
+            //Try get Action Config
+
+            if(ConfigUtil.TryGetAction(CandidApiManager.Instance.WORLD_CANISTER_ID, actionId, out var actionConfig) == false)
+            {
+                $"Action of Id: {actionId} could not be found".Error(typeof(LuckyWheelUI).Name);
+                throw new();
+            }
+
+            var callerActionConfig = actionConfig.callerAction;
+
+            if(callerActionConfig == null)
+            {
+                $"callerAction is null".Warning(typeof(LuckyWheelUI).Name);
+                throw new();
+            }
+
+            var callerOutcomesConfig = callerActionConfig.Outcomes;
+
+            if (callerOutcomesConfig.Count < 2)
+            {
+                $"Action is missing Wheel of Fortune's Outcome".Warning(typeof(LuckyWheelUI).Name);
+                throw new();
+            }
+
+            var possibleWheelOfFortuneOutcomesConfig = callerOutcomesConfig[1];
+
+
+            //ACtion Arguments Setup
+
+            var currentKittyId = "kitty_0";
+            var currentKityHealth = 40;
+            var totalBattleXp = 120;
+
+            List<Field> args = new() 
+            {
+                 new Field("kitty_id", currentKittyId),
+                  new Field("new_health", $"{currentKityHealth}"),
+                   new Field("total_battle_xp", $"{totalBattleXp}"),
+            };
+
+            //Process Action
+
+            var actionResult = await ActionUtil.ProcessAction(actionId, args);
+
+            if (actionResult.IsErr)
+            {
+                $"{actionResult.AsErr()}".Warning(typeof(LuckyWheelUI).Name);
+                throw new();
+            }
+
+            var actionResultAsOk = actionResult.AsOk();
+
+            var callerActionOutcome = actionResultAsOk.callerOutcomes;
+
+            var rewardName = "";
+            var rewardAmount = 0.0;
+
+            //Try filter from all the applied outcomes the one for the Wheel of Fortune
+
+            foreach (var posibleOutcome in possibleWheelOfFortuneOutcomesConfig.PossibleOutcomes)
+            {
+                //If update is not of type entity then skip
+                if(posibleOutcome.Option.Tag != ActionOutcomeOption.OptionInfoTag.UpdateEntity)
+                {
+                    continue;
+                }
+
+                var outcomeEntityConfig = posibleOutcome.Option.AsUpdateEntity();
+
+                foreach (var actionCallerEntityOutcome in callerActionOutcome.entityEdits)
+                {
+                    //If one of the expected Wheel of Fortune outcomes is found on the applied outcomes then pick the one that matches
+                    if(outcomeEntityConfig.Eid == actionCallerEntityOutcome.Value.eid)
+                    {
+                        rewardName = actionCallerEntityOutcome.Value.eid;
+
+                        if(actionCallerEntityOutcome.Value.fields.TryGetValue("amount", out EntityFieldEdit.Base amountEdit) == false)
+                        {
+                            $"Entity of id: {rewardName} doesn't has field of id: amount".Warning(typeof(LuckyWheelUI).Name);
+                            throw new();
+                        }
+
+                        if(amountEdit is not EntityFieldEdit.IncrementNumber amountIncrementEdit)
+                        {
+                            $"Issue casting value".Warning(typeof(LuckyWheelUI).Name);
+                            throw new();
+                        }
+
+                        if (amountIncrementEdit.Value is not EntityFieldEdit.IncrementNumber.ValueType.Number rewardAmountObj)
+                        {
+                            $"Issue casting value".Warning(typeof(LuckyWheelUI).Name);
+                            throw new();
+                        }
+                        
+                        rewardAmount = rewardAmountObj.Value;
+                        goto breakPossibleOutcomesLoop;
+                    }
+                }
+
+                breakPossibleOutcomesLoop : 
+                {
+                    break;
+                }
+            }
+
+
+            //Get reward by name
+            choosenReward = LuckyWheelRewardSO.Get(rewardName);
+
         }
         catch
         {
-            _rewardId = 1;
+            choosenReward = LuckyWheelRewardSO.Get(1);
         }
-        choosenReward = LuckyWheelRewardSO.Get(_rewardId);
+
+        choosenReward = LuckyWheelRewardSO.Get(1);
         if (requestedToSeeReward)
         {
             Setup();
