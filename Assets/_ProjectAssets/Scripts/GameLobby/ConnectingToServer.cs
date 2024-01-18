162b10fc26e956378dfa08021fa4a334f1d372a1
Filip <filpkrcmarevic555@gmail.com>
2024-01-18 19:05:41 +0100

    Removed support for stoic and metamask wallets

--- Assets/_ProjectAssets/Scripts/GameLobby/ConnectingToServer.cs
+++ Assets/_ProjectAssets/Scripts/GameLobby/ConnectingToServer.cs
@@ -1,9 +1,6 @@
 using Boom;
 using Boom.Patterns.Broadcasts;
 using Boom.Utility;
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ConnectingToServer : MonoBehaviour
@@ -17,26 +14,19 @@ public class ConnectingToServer : MonoBehaviour
 
     [SerializeField] private GameObject loginFailed;
 
-    //NEW
     private void Awake()
     {
         UserUtil.AddListenerMainDataChange<MainDataTypes.LoginData>(LoginDataChangeHandler);
         UserUtil.AddListenerDataChangeSelf<DataTypes.NftCollection>(NftDataChangeHandler);
-        //UserUtil.UpdateDataSelf<DataTypes.NftCollection>(new DataTypes.NftCollection[0]);
     }
 
     private void OnDestroy()
     {
-        //ExternalJSCommunication.Instance.onWalletConnected -= OnWalletConnected;
-        //ExternalJSCommunication.Instance.onNFTsReceived -= OnNFTsReceived;
-
-        //NEW
         UserUtil.RemoveListenerMainDataChange<MainDataTypes.LoginData>(LoginDataChangeHandler);
         UserUtil.RemoveListenerDataChangeSelf<DataTypes.NftCollection>(NftDataChangeHandler);
     }
 
 
-    //NEW
     private void LoginDataChangeHandler(MainDataTypes.LoginData data)
     {
         if (data.asAnon)
@@ -46,12 +36,8 @@ public class ConnectingToServer : MonoBehaviour
         }
         var text = logText.GetComponent<TMPro.TextMeshProUGUI>();
         text.text = "Connection made. Waiting for NFTs...";
-
-
-
     }
 
-    //NEW
     private void NftDataChangeHandler(Data<DataTypes.NftCollection> data)
     {
         if (UserUtil.IsDataValidSelf<DataTypes.NftCollection>() == false) return;
@@ -60,26 +46,9 @@ public class ConnectingToServer : MonoBehaviour
 
         $"Setup ICKitties Nft Collection".Log(typeof(ConnectingToServer).Name);
 
-        GameState.nfts.Clear();
-
         GameState.nfts.Add(new NFT() { imageUrl = "https://rw7qm-eiaaa-aaaak-aaiqq-cai.raw.ic0.app/?&tokenid=hvtag-6ykor-uwiaa-aaaaa-cqace-eaqca-aaabd-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://images.entrepot.app/tnc/rw7qm-eiaaa-aaaak-aaiqq-cai/jairp-5ykor-uwiaa-aaaaa-cqace-eaqca-aacdq-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://images.entrepot.app/tnc/rw7qm-eiaaa-aaaak-aaiqq-cai/4gya5-nakor-uwiaa-aaaaa-cqace-eaqca-aaabu-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://images.entrepot.app/tnc/rw7qm-eiaaa-aaaak-aaiqq-cai/jxrpt-fikor-uwiaa-aaaaa-cqace-eaqca-aaajx-q" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://images.entrepot.app/tnc/rw7qm-eiaaa-aaaak-aaiqq-cai/hs6xe-7ikor-uwiaa-aaaaa-cqace-eaqca-aaaad-q" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://images.entrepot.app/tnc/rw7qm-eiaaa-aaaak-aaiqq-cai/ivtko-hikor-uwiaa-aaaaa-cqace-eaqca-aaaql-q" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://images.entrepot.app/tnc/rw7qm-eiaaa-aaaak-aaiqq-cai/xtmbl-nqkor-uwiaa-aaaaa-cqace-eaqca-aaacj-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://rw7qm-eiaaa-aaaak-aaiqq-cai.raw.ic0.app/?tokenid=cfbp6-cikor-uwiaa-aaaaa-cqace-eaqca-aadkb-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://rw7qm-eiaaa-aaaak-aaiqq-cai.raw.ic0.app/?tokenid=v3yzq-4ykor-uwiaa-aaaaa-cqace-eaqca-aaa5u-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://rw7qm-eiaaa-aaaak-aaiqq-cai.raw.ic0.app/?tokenid=e7yl4-fqkor-uwiaa-aaaaa-cqace-eaqca-aaeeg-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://rw7qm-eiaaa-aaaak-aaiqq-cai.raw.ic0.app/?tokenid=4cp2w-bykor-uwiaa-aaaaa-cqace-eaqca-aacz4-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://rw7qm-eiaaa-aaaak-aaiqq-cai.raw.ic0.app/?tokenid=jairp-5ykor-uwiaa-aaaaa-cqace-eaqca-aacdq-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://rw7qm-eiaaa-aaaak-aaiqq-cai.raw.ic0.app/?tokenid=xtmbl-nqkor-uwiaa-aaaaa-cqace-eaqca-aaacj-a" });
-        GameState.nfts.Add(new NFT() { imageUrl = "https://rw7qm-eiaaa-aaaak-aaiqq-cai.raw.ic0.app/?tokenid=4gya5-nakor-uwiaa-aaaaa-cqace-eaqca-aaabu-a" });
-
 
         //Look for the ICKitties Collection and  add the user's nft images to the GameState.nfts
-
         foreach (var keyValue in data.elements)
         {
             var collection = keyValue.Value;
@@ -104,12 +73,12 @@ public class ConnectingToServer : MonoBehaviour
             $"{loginDataResult.AsErr()}".Error(typeof(ConnectingToServer).Name);
             return;
         }
+        
+        GameState.principalId = UserUtil.GetPrincipal();
 
         var loginDataAsOk = loginDataResult.AsOk();
 
         //Connect to firebase
-
-        ExternalJSCommunication.Instance.WalletConnected();
         FirebaseManager.Instance.TryLoginAndGetData(loginDataAsOk.principal, OnLoginFinished);
     }
 
@@ -119,29 +88,21 @@ public class ConnectingToServer : MonoBehaviour
         Broadcast.Invoke<UserLogout>();
     }
 
-    //NEW
-    public void LogIn()
-    {
-        Broadcast.Invoke<UserLoginRequest>();
-    }
-
     public void ConnectToWallet()
     {
-        //NEW
         LogIn();
 
-        ExternalJSCommunication.Instance.TryConnectWallet();
         connectButton.SetActive(false);
         logText.SetActive(true);
 
         var text = logText.GetComponent<TMPro.TextMeshProUGUI>();
         text.text = "Waiting the connection with ICP Wallet to be approved...";
-
-        //ExternalJSCommunication.Instance.onWalletConnected += OnWalletConnected;
-        //ExternalJSCommunication.Instance.onNFTsReceived += OnNFTsReceived;
     }
-
-
+    
+    private void LogIn()
+    {
+        Broadcast.Invoke<UserLoginRequest>();
+    }
 
     private void OnLoginFinished(bool _result)
     {
@@ -157,23 +118,4 @@ public class ConnectingToServer : MonoBehaviour
         }
     }
 
-
-
-
-    //NO LONGER NEEDED
-    public void OnWalletConnected()
-    {
-        //ExternalJSCommunication.Instance.onWalletConnected -= OnWalletConnected;
-
-        var text = logText.GetComponent<TMPro.TextMeshProUGUI>();
-        text.text = "Connection made. Waiting for NFTs...";
-    }
-
-    //NO LONGER NEEDED
-    public void OnNFTsReceived()
-    {
-        ExternalJSCommunication.Instance.onNFTsReceived -= OnNFTsReceived;
-        GameState.walletId = "asd";
-        FirebaseManager.Instance.TryLoginAndGetData(GameState.principalId, OnLoginFinished);
-    }
 }
